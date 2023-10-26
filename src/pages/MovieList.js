3106e4e5c569c81438db0213e37d8378f0094b1b
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-26 09:51:34 +0800

    01tailwind

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,9 @@
 export const MovieList = () => {
   return (
     <main>
-      <div>MovieList</div>
+      <div className="text-2xl bg-sky-200 p-10 border-8 border-blue-700 rounded-lg m-5">
+        MovieList1
+      </div>
     </main>
   );
 };
