90ffc4e72063cb8303bbc9af9fbb038424f36dba
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-26 10:23:02 +0800

    02uidesign

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-2xl bg-sky-200 p-10 border-8 border-blue-700 rounded-lg m-5">
-        MovieList1
-      </div>
+      <div>MovieList1</div>
     </main>
   );
 };
