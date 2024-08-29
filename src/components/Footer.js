6c1ca16a1a7f51fb459569920dbf75b1ea741ecf
rbazur <razur@udm.edu.ph>
2024-08-29 04:23:17 -0700

    03navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,12 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{' '}
-          <Link
-            to="/"
-            className="hover:underline"
-          >
+          © 2024{" "}
+          <Link to="/" className="hover:underline">
             CineZone
           </Link>
           . All Rights Reserved.
